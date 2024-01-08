7ea997d5e0eda8df3bbbe0f7d59a73cae33f7c8b
John Jorgensen <john@jorgensenfamily.us>
2024-01-07 18:58:01 -0800

    add form data loading

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function submit() {
-    console.log("submit");
+    const formData = new FormData(document.getElementById("oldspice"));
+    for (const entry of formData.entries()) {
+        console.log(entry)
+    }
+    const form = Object.fromEntries(formData);
+    console.log(form);
 }
